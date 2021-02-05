9ba5e03e371b64400c8b87ac7c3381f921725da4
dantefleschpereira <48799984+dantefleschpereira@users.noreply.github.com>
2021-02-05 10:04:07 -0300

    implementação rep SDJ customizado

--- src/main/java/com/fleschworks/food/domain/repository/RestauranteRepository.java
+++ src/main/java/com/fleschworks/food/domain/repository/RestauranteRepository.java
@@ -12,7 +12,7 @@ import org.springframework.stereotype.Repository;
 import com.fleschworks.food.domain.model.Restaurante;
 
 @Repository
-public interface RestauranteRepository extends JpaRepository<Restaurante, Long> {
+public interface RestauranteRepository extends JpaRepository<Restaurante, Long>, RestauranteRepositoryQueries {
 	
 	List<Restaurante> queryByTaxaFreteBetween (BigDecimal taxaInicial, BigDecimal taxaFinal);
 	
@@ -26,5 +26,5 @@ public interface RestauranteRepository extends JpaRepository<Restaurante, Long> 
 	List<Restaurante> findTop2ByNomeContaining(String nome);
 	
 	int countByCozinhaId(Long cozinha);
-	
+		
 }
