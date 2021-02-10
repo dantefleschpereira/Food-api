3b35e37a9ab135727118b62f8463ebe056360681
dantefleschpereira <48799984+dantefleschpereira@users.noreply.github.com>
2021-02-09 21:35:01 -0300

    inclusão LocalDateTime

--- src/main/java/com/fleschworks/food/domain/model/Restaurante.java
+++ src/main/java/com/fleschworks/food/domain/model/Restaurante.java
@@ -1,6 +1,7 @@
 package com.fleschworks.food.domain.model;
 
 import java.math.BigDecimal;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -14,6 +15,9 @@ import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
@@ -33,8 +37,20 @@ public class Restaurante {
 	@Column(nullable = false)
 	private String nome;
 	
+	@JsonIgnore
 	@Embedded
 	private Endereco endereco;
+	
+	@JsonIgnore
+	@CreationTimestamp
+	@Column (nullable = false, columnDefinition = "datetime")
+	private LocalDateTime dataCadastro;
+	
+	@JsonIgnore
+	@UpdateTimestamp
+	@Column(nullable = false, columnDefinition = "datetime")
+	private LocalDateTime dataAtualizacao;
+
 
 	@Column(name = "taxa_frete", nullable = false)
 	private BigDecimal taxaFrete;
